4230bc23f028eb21d05e15fa8690289a10e58379
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2022-02-12 23:33:17 +0300

    footer logo added & layout page is updated

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 import Nav from './Nav'
+import styles from "../styles/Home.module.css";
 
 export default function Layout({ children }) {
     return (
         <div className='flex flex-col justify-between min-h-screen'>
             <Nav />
             <main>{children}</main>
-            <footer>FOOTER</footer>
+            <footer className={styles.footer}>
+                <a
+                    href="https://github.com/serifcolakel"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <p>Powered by</p>
+                    <span className="flex items-center justify-center pl-2" >
+                        <Image src="/logo.png" alt="Serif Logo" width={122} height={36} />
+                    </span>
+                </a>
+            </footer>
         </div>
     )
 }
